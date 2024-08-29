e5729a563411cbd02bf6ccecedd6e3bc042d4445
Daniil Kolomiiets <d.kolomiiets@room8group.com>
2024-08-29 15:36:28 +0300

    small fix

--- Engine/EngineSource/source/Graphics/Renderer.cpp
+++ Engine/EngineSource/source/Graphics/Renderer.cpp
@@ -413,15 +413,10 @@ void Engine::Renderer::Render(Camera* camera)
 	ID3D11ShaderResourceView* const nullSRV = { NULL };
 	context->PSSetShaderResources(30u, 1u, &nullSRV);
 
-
-
 	pSkyBox->BindSkyBox(2u);
 	pSkyBox->Draw();
 
-
-	context->OMSetDepthStencilState(pDSState.Get(), 0u);
 	RenderParticles(camera);
-	RenderParticles(camera,0.0166667f);
 
 	ID3D11ShaderResourceView* const pSRV[3] = { NULL, NULL, NULL };
 	context->PSSetShaderResources(11, 3u, pSRV);
